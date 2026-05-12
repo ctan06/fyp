db2e27a1ce02644fa661954a69d985bd4a2ae30e
Jad Radwan <jad.radwan@outlook.com>
2026-05-12 14:10:16 +0300

    fixed a styling issue in the nav bar and changed the name of our project in the about section from fyp to its real name

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -11,13 +11,13 @@ const About = () => {
             <h3>ABOUT US</h3>
             <h2>Building Confidence in Network Configuration</h2>
             <p>
-                FYP is a web-based platform designed to help organizations monitor and 
+                Network Configuration Checker is a web-based platform designed to help organizations monitor and 
                 validate their network configurations with ease. By analyzing router settings 
                 against predefined standards, we help identify misconfigurations and ensure network 
                 reliability and security.
             </p>
             <p>
-                Developed as a final year computer science project, FYP focuses on clarity, 
+                Developed as a final year computer science project, Network Configuration Checker focuses on clarity, 
                 accuracy, and scalability. Our goal is to simplify network compliance checks and 
                 provide actionable insights that empower users to maintain robust and 
                 well-configured networks.
